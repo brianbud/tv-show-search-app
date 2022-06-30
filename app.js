c0c7c333ae7a8f3677f114dd309322b63af87907
Brian Bud <brian@brianbud.com>
2022-06-29 21:23:50 -0400

    get input value

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('#searchForm');
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-  console.log('Testing');
+  const searchTerm = form.elements.query.value;
 });
