89737b042fd6faf9ab48df1f817fe79a1b785c05
Brian Bud <brian@brianbud.com>
2022-07-03 13:19:07 -0400

    find image of first obj and append img to webpage

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ form.addEventListener('submit', async function (event) {
   const response = await axios.get(
     `https://api.tvmaze.com/search/shows?q=${searchTerm}`
   );
-  console.log(response.data);
+  console.log(response.data[0].show.image.medium);
+  const img = document.createElement('img');
+  img.src = response.data[0].show.image.medium;
+  document.body.appendChild(img);
 });
