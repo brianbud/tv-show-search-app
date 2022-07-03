7e68a21a7f6eb5a5066f31545d1c4d661e446b70
Brian Bud <brian@brianbud.com>
2022-07-03 13:40:36 -0400

    empty search input after every query search

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ form.addEventListener('submit', async function (event) {
     `https://api.tvmaze.com/search/shows?q=${searchTerm}`
   );
   displayImages(response.data);
+  form.elements.query.value = '';
 });
 
 const displayImages = (shows) => {
   for (let result of shows) {
-    console.log(result);
-    const img = document.createElement('img');
-    img.src = result.show.image.medium;
-    document.body.appendChild(img);
+    if (result.show.image) {
+      const img = document.createElement('img');
+      img.src = result.show.image.medium;
+      document.body.appendChild(img);
+    }
   }
 };
