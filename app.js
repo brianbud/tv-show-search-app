c32c63ca81239ae7302c72a69bb258ebbe26ed63
Brian Bud <brian@brianbud.com>
2022-07-03 13:14:13 -0400

    async await and display res data

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', async function (event) {
   event.preventDefault();
   const searchTerm = form.elements.query.value;
+  const response = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+  );
+  console.log(response.data);
 });
